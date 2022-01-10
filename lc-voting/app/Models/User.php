da5054290b5299f3093448ee330042d317001873
BasharAlZawahreh <zawahrehbashar@gmail.com>
2022-01-10 15:17:12 +0200

    Gravatar Images

--- lc-voting/app/Models/User.php
+++ lc-voting/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Hamcrest\Type\IsNumeric;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -47,4 +48,18 @@ class User extends Authenticatable
         return $this->hasMany(Idea::class);
     }
 
+    public function getAvatar()
+    {
+        // $randomInt = rand(1, 36);
+        $firstChar = $this->email[0];
+        $integerToUse = is_numeric($firstChar) ? ord(strtolower($firstChar)) - 21 : ord(strtolower($firstChar)) - 96;
+
+
+        return 'https://www.gravatar.com/avatar/'
+            . md5($this->email)
+            . '?s=200'
+            . '&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+            . $integerToUse
+            . '.png';
+    }
 }
