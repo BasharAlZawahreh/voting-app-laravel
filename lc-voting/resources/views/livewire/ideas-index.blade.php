f9a7b8349a5711f199da67c6622363c07965e609
BasharAlZawahreh <zawahrehbashar@gmail.com>
2022-01-17 07:02:11 +0200

    Search Filter

--- lc-voting/resources/views/livewire/ideas-index.blade.php
+++ lc-voting/resources/views/livewire/ideas-index.blade.php
@@ -9,30 +9,36 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+            <select wire:model="filter" name="other_filters" id="other_filters"
+                class="w-full rounded-xl border-none px-4 py-2">
                 <option value="No Filter">Filter One</option>
                 <option value="Top Voted">Top Voted</option>
                 <option value="My Ideas">My Ideas</option>
             </select>
         </div>
         <div class="w-full md:w-2/3 relative">
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
+            <input wire:model="search" type="search" placeholder="Find an idea"
+                class="w-full rounded-xl bg-white border-none placeholder-gray-900 px-4 py-2 pl-8">
             <div class="absolute top-0 flex itmes-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </div>
         </div>
     </div> <!-- end filters -->
 
     <div class="ideas-container space-y-6 my-8">
-        @foreach ($ideas as $idea)
-            <livewire:idea-index
-                :key="$idea->id"
-                :idea="$idea"
-                :votesCount="$idea->votes_count"
-            />
-        @endforeach
+        @forelse ($ideas as $idea)
+            <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count" />
+        @empty
+            <div class="mx-auto w-70 mt-12">
+                <img src="{{asset('img/no-ideas.svg')}}" alt="No Idea was found!">
+                <div>
+                    No Idea was found!
+                </div>
+            </div>
+        @endforelse
     </div> <!-- end ideas-container -->
 
     <div class="my-8">
