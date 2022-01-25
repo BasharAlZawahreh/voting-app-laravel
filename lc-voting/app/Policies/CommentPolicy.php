d5e6e26eaa0cfb68211417b8bd325b253748869e
BasharAlZawahreh <zawahrehbashar@gmail.com>
2022-01-25 14:55:24 +0200

    Comment migration and model

--- lc-voting/app/Policies/CommentPolicy.php
+++ lc-voting/app/Policies/CommentPolicy.php
@@ -65,7 +65,7 @@ class CommentPolicy
      */
     public function delete(User $user, Comment $comment)
     {
-        //
+       //return auth()->check() && (auth()->user()->isAdmin() || $comment->user_id === auth()->id());
     }
 
     /**
