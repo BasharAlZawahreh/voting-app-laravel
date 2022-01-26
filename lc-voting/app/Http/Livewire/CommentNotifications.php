b5bccba355d055a1287ebd9a9de47723db19328e
BasharAlZawahreh <zawahrehbashar@gmail.com>
2022-01-26 15:08:17 +0200

    BadgeCount and Skeloton Loader

--- lc-voting/app/Http/Livewire/CommentNotifications.php
+++ lc-voting/app/Http/Livewire/CommentNotifications.php
@@ -6,18 +6,38 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFICATIONS_THRESHOLD = 20;
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
 
     protected $listeners = ['getNotifications'];
 
     public function mount()
     {
         $this->notifications = collect([]);
+        $this->getNotificationCount();
+        $this->isLoading = true;
     }
 
     public function getNotifications()
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()->user()
+            ->unreadNotifications()
+            ->latest()
+            ->take(self::NOTIFICATIONS_THRESHOLD)
+            ->get();
+
+            $this->isLoading = false;
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if ($this->notificationCount > self::NOTIFICATIONS_THRESHOLD) {
+            $this->notificationCount = self::NOTIFICATIONS_THRESHOLD . "+";
+        }
     }
 
     public function render()
